2d413d09f700752a923c102885b0a4c346a6085a
Kostya <dreamfall551@ya.ru>
2024-12-21 18:43:26 +0300

    finaly edit scripts and cfg for ansible

--- ansible/invscript.py
+++ ansible/invscript.py
@@ -5,27 +5,49 @@ import os
 with open('../terraform/terraform_output.json') as f:  # Путь относительно директории с Python-скриптом
     terraform_output = json.load(f)
 
+# Шаг 2: Формирование структуры инвентаря
 inventory = {
     "all": {
-        "hosts": list(terraform_output.keys()),
-        "vars": {}
-    },
-    "_meta": {
-        "hostvars": {
-            vm: {
-                "ansible_host": ip,
-                "ansible_user": "root" 
-            } for vm, ip in terraform_output.items()
+        "children": {
+            "nginx": {
+                "hosts": {}
+            },
+            "proxy": {
+                "hosts": {}
+            }
         }
     }
 }
 
-# Шаг 3: Сохранение инвентаря в файл JSON для Ansible
-inventory_dir = '../ansible'
-inventory_file_path = os.path.join(inventory_dir, 'inventory.json') # Путь будет внутри директории с Ansible-скриптом
+private_key = "/home/dream/.ssh/git"  # Укажите путь к приватному ключу
 
+# Шаг 3: Заполнение данных для каждой группы
+for host, ip in terraform_output.items():
+    if "proxy" in host:
+        inventory["all"]["children"]["proxy"]["hosts"][host] = {
+            "ansible_host": ip,
+            "ansible_user": "root",
+            "ansible_ssh_private_key_file": private_key,
+            "ansible_ssh_common_args": "-o StrictHostKeyChecking=no"
+
+        }
+    elif "nginx" in host:
+        inventory["all"]["children"]["nginx"]["hosts"][host] = {
+            "ansible_host": ip,
+            "ansible_user": "root",
+            "ansible_ssh_private_key_file": private_key,
+            "ansible_ssh_common_args": "-o StrictHostKeyChecking=no"
+
+        }
+
+# Шаг 4: Сохранение инвентаря в файл JSON
+inventory_dir = '../ansible'  # Директория, куда сохраняем инвентарь
+inventory_file_path = os.path.join(inventory_dir, 'inventory.json')  # Путь к файлу
+
+# Сохраняем инвентарь в файл
 with open(inventory_file_path, 'w') as f:
     json.dump(inventory, f, indent=4)
 
+# Выводим сообщение о завершении
 print(f"Инвентарь для Ansible был успешно создан и сохранен в {inventory_file_path}")
 
